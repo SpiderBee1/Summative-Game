21c8ee91b4c2e3ca3873bb4a6f98117926fb58f1
goodm <goodm@DESKTOP-JAID4B4>
2018-01-10 11:29:18 -0500

    version0.3

--- src/kickout/combat/DrawingExample.java
+++ src/kickout/combat/DrawingExample.java
@@ -4,13 +4,12 @@ package kickout.combat;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 import java.awt.Dimension;
 import java.awt.Graphics;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import java.awt.Color;
+import java.awt.Rectangle;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
@@ -26,30 +25,31 @@ public class DrawingExample extends JComponent {
     // Height and Width of our game
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
-    
+
     //Title of the window
-    String title = "My Game";
+    String title = "ball test";
 
     // sets the framerate and delay for our game
     // you just need to select an approproate framerate
     long desiredFPS = 60;
     long desiredTime = (1000) / desiredFPS;
 
-
     // YOUR GAME VARIABLES WOULD GO HERE
-    int pacmanx = 90;
+    Rectangle ball = new Rectangle(90, 500, 50, 50);
     int rotation = 0;
+    int rotationOffset = 0;
     int charge = 180;
+    int ballxSpeed = 0;
+    boolean ballOnGround = true;
     boolean leftButton = false;
     boolean rightButton = false;
     boolean upButton = false;
     boolean downButton = false;
     // GAME VARIABLES END HERE   
 
-    
     // Constructor to create the Frame and place the panel in
     // You will learn more about this in Grade 12 :)
-    public DrawingExample(){
+    public DrawingExample() {
         // creates a windows to show my game
         JFrame frame = new JFrame(title);
 
@@ -64,52 +64,51 @@ public class DrawingExample extends JComponent {
         frame.pack();
         // shows the window to the user
         frame.setVisible(true);
-        
+
         // add listeners for keyboard and mouse
         frame.addKeyListener(new Keyboard());
         Mouse m = new Mouse();
-        
+
         this.addMouseMotionListener(m);
         this.addMouseWheelListener(m);
         this.addMouseListener(m);
     }
-    
+
     // drawing of the game happens in here
     // we use the Graphics object, g, to perform the drawing
-    // NOTE: This is already double buffered!(helps with framerate/speed)
+    // NOTE: This is already double buffered!(helps with framerate/sped)
     @Override
     public void paintComponent(Graphics g) {
         // always clear the screen first!
         g.clearRect(0, 0, WIDTH, HEIGHT);
-        
+
         int[] xpoints = {550, 600, 750};
         int[] ypoints = {175, 50, 130};
         // GAME DRAWING GOES HERE
         g.setColor(Color.WHITE);
-        g.fillOval(pacmanx, 500, 70, 70);
+        g.fillOval(ball.x, ball.y, ball.width, ball.height);
         g.setColor(Color.MAGENTA);
-        g.fillArc(pacmanx, 500, 70, 70, (rotation*-1)-charge/2, charge);
+        g.fillRect(50, 50, 50, 50);
+        g.fillArc(ball.x, ball.y, ball.width, ball.height, (rotation * -1) - charge / 2, charge);
         g.setColor(Color.BLACK);
-        g.drawOval(pacmanx, 500, 70, 70);
-        if(charge>360){
+        g.drawOval(ball.x, ball.y, ball.width, ball.height);
+        if (charge > 360) {
             g.setColor(Color.MAGENTA);
-        }else{
-        g.setColor(Color.WHITE);
+        } else {
+            g.setColor(Color.WHITE);
         }
-        g.fillOval(pacmanx + 10, 510, 50, 50);
+        g.fillOval(ball.x + 10, 510, 30, 30);
         g.setColor(Color.BLACK);
-        g.drawOval(pacmanx + 10, 510, 50, 50);
+        g.drawOval(ball.x + 10, 510, 30, 30);
 
         // GAME DRAWING ENDS HERE
     }
 
-
     // This method is used to do any pre-setup you might need to do
     // This is run before the game loop begins!
-    public void  preSetup(){
-       // Any of your pre setup before the loop starts should go here
+    public void preSetup() {
+        // Any of your pre setup before the loop starts should go here
 
-       
     }
 
     // The main game loop
@@ -131,20 +130,59 @@ public class DrawingExample extends JComponent {
 
             // all your game rules and move is done in here
             // GAME LOGIC STARTS HERE 
-            //make pacman move
-            if(leftButton){
-               pacmanx = pacmanx-2;
+            //make ball move
+            if (leftButton) {
+                if (ballxSpeed > -20) {
+                    ballxSpeed = ballxSpeed - 2;
+                }
             }
-            if(rightButton){
-                pacmanx = pacmanx+2;
+            if (rightButton) {
+                if (ballxSpeed < 20) {
+                    ballxSpeed = ballxSpeed + 2;
+                }
             }
-            if(upButton){
-                charge = charge +2;
+            if (upButton) {
+                charge = charge + 2;
             }
-            if(downButton){
+            if (downButton) {
                 charge = charge - 2;
             }
-            rotation = pacmanx;
+            //stops the ball from travelling offscreen to the right
+            if (ball.x + ball.width >= WIDTH) {
+               ballxSpeed = ballxSpeed/2;
+                if (!ballOnGround){
+                    ballxSpeed = ballxSpeed +2;
+                }
+                if (ballxSpeed > 0) {
+                    ballxSpeed = ballxSpeed * (-1);
+                }
+                rotationOffset = rotationOffset - (ballxSpeed - 5);
+                
+                ball.x = WIDTH-ball.width-1;
+            }
+            //stops the ball from travelling offscreen to the left
+            if (ball.x <= 0) {
+                ballxSpeed = ballxSpeed/2;
+                if (!ballOnGround){
+                    ballxSpeed = ballxSpeed -2;
+                }
+                if (ballxSpeed < 0) {
+                    ballxSpeed = ballxSpeed * (-1);
+                }
+                rotationOffset = rotationOffset - (ballxSpeed + 5);
+                
+                ball.x = 1;
+            }
+            //BALL VISUALS
+            //prevents offset number overflow (very unlikely occurence but safer this way)
+            if (rotationOffset > 360) {
+                rotationOffset = rotationOffset - 360;
+            }
+            if (rotationOffset < -360) {
+                rotationOffset = rotationOffset + 360;
+            }
+            ball.x = ball.x + ballxSpeed;
+            rotation = rotationOffset + ball.x;
             // GAME LOGIC ENDS HERE 
             // update the drawing (calls paintComponent)
             repaint();
@@ -165,40 +203,40 @@ public class DrawingExample extends JComponent {
         }
     }
 
-    
-
     // Used to implement any of the Mouse Actions
     private class Mouse extends MouseAdapter {
+
         // if a mouse button has been pressed down
         @Override
-        public void mousePressed(MouseEvent e){
-            
+        public void mousePressed(MouseEvent e) {
+
         }
-        
+
         // if a mouse button has been released
         @Override
-        public void mouseReleased(MouseEvent e){
-            
+        public void mouseReleased(MouseEvent e) {
+
         }
-        
+
         // if the scroll wheel has been moved
         @Override
-        public void mouseWheelMoved(MouseWheelEvent e){
-            
+        public void mouseWheelMoved(MouseWheelEvent e) {
+
         }
 
         // if the mouse has moved positions
         @Override
-        public void mouseMoved(MouseEvent e){
-            
+        public void mouseMoved(MouseEvent e) {
+
         }
     }
-    
+
     // Used to implements any of the Keyboard Actions
-    private class Keyboard extends KeyAdapter{
+    private class Keyboard extends KeyAdapter {
+
         // if a key has been pressed down
         @Override
-        public void keyPressed(KeyEvent e){
+        public void keyPressed(KeyEvent e) {
             int key = e.getKeyCode();
 
             if (key == KeyEvent.VK_LEFT) {
@@ -214,10 +252,10 @@ public class DrawingExample extends JComponent {
                 downButton = true;
             }
         }
-        
+
         // if a key has been released
         @Override
-        public void keyReleased(KeyEvent e){
+        public void keyReleased(KeyEvent e) {
             int key = e.getKeyCode();
 
             if (key == KeyEvent.VK_LEFT) {
@@ -234,17 +272,15 @@ public class DrawingExample extends JComponent {
             }
         }
     }
-    
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // creates an instance of my game
         DrawingExample game = new DrawingExample();
-                
+
         // starts the game loop
         game.run();
     }
 }
-
